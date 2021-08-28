575f06a5ae684d40360e777cbcf112806282968f
btriz <00btrz@gmail.com>
2021-08-28 17:39:39 -0300

    Updates

---
+++ precache-manifest.ee6a0bc217730cef815d45139013f55f.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "329207df0460e723d38522530bbf5268",
+    "url": "/Btriz.github.io/frontend-online-store/index.html"
+  },
+  {
+    "revision": "6a92e72dafdfa682b242",
+    "url": "/Btriz.github.io/frontend-online-store/static/css/main.e603e046.chunk.css"
+  },
+  {
+    "revision": "8b3a0d0b24180e0f8417",
+    "url": "/Btriz.github.io/frontend-online-store/static/js/2.8573a162.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/Btriz.github.io/frontend-online-store/static/js/2.8573a162.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a92e72dafdfa682b242",
+    "url": "/Btriz.github.io/frontend-online-store/static/js/main.5efcd73e.chunk.js"
+  },
+  {
+    "revision": "913fed91848de3e26431",
+    "url": "/Btriz.github.io/frontend-online-store/static/js/runtime-main.45e8c13f.js"
+  },
+  {
+    "revision": "76022c59f1b26208409b85ad435ff7fe",
+    "url": "/Btriz.github.io/frontend-online-store/static/media/clip-1082.76022c59.png"
+  },
+  {
+    "revision": "590851dbee6674f6c258c6420152b9c9",
+    "url": "/Btriz.github.io/frontend-online-store/static/media/clip-1670.590851db.png"
+  },
+  {
+    "revision": "29ad785578024acd461e07226be46668",
+    "url": "/Btriz.github.io/frontend-online-store/static/media/logo.29ad7855.png"
+  },
+  {
+    "revision": "59f1c25b6380ec9e3270e0f99f909f83",
+    "url": "/Btriz.github.io/frontend-online-store/static/media/urban-681.59f1c25b.png"
+  },
+  {
+    "revision": "4b976e2bc0b1471f26658ed4e7831524",
+    "url": "/Btriz.github.io/frontend-online-store/static/media/urban-867.4b976e2b.png"
+  },
+  {
+    "revision": "1d3bb1886af3a6953b424b40889999d7",
+    "url": "/Btriz.github.io/frontend-online-store/static/media/urban-890.1d3bb188.png"
+  },
+  {
+    "revision": "fb6cc4096b4191ee03f87accb3157932",
+    "url": "/Btriz.github.io/frontend-online-store/static/media/urban-912.fb6cc409.png"
+  }
+]);
